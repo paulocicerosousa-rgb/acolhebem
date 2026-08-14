487d788ae1d2a90730525028c39e39ff0aaa7afb
paulocicerosousa-rgb <paulocicerosousa@gmail.com>
2026-08-14 17:29:01 -0300

    Update _sheets.js

--- api/_sheets.js
+++ api/_sheets.js
@@ -1,7 +1,7 @@
 const { google } = require('googleapis');
 
 const auth = new google.auth.JWT(
-  process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  process.env.GOOGLE_CLIENT_EMAIL,
   null,
   process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   ['https://www.googleapis.com/auth/spreadsheets']
